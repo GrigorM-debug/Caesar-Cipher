f09b19a2a6fe99eedf8a6fae8b60de5404fe39c1
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-07-05 18:31:19 +0300

    Implemented user input validations

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿Console.Write("Enter your secret message: ");
+string secret = Console.ReadLine();
+
+while(string.IsNullOrEmpty(secret) || string.IsNullOrWhiteSpace(secret))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Secret message can't be null or empty !");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("Enter your secret message: ");
+    secret = Console.ReadLine();
+}
+
+Console.Write("Enter your secret key (0-26): ");
+int key = int.Parse(Console.ReadLine());
+
+while (key < 0 || key > 26)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Secret key number must be between 0 and 26 !");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("Enter your secret key (0-26): ");
+    key = int.Parse(Console.ReadLine());
+}
+
+Console.Write("> Encryption (0) or Decryption (1)?: ");
+int toggle = int.Parse(Console.ReadLine());
+
+while (toggle < 0 || toggle > 1)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Your choose can be only 0 or 1 !");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("> Encryption (0) or Decryption (1)?: ");
+    toggle = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(secret);
+Console.WriteLine(key);
+
+Console.WriteLine();
+
