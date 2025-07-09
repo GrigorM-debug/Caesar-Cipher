8dc9fff27ebc6b418c06063f9462f50988dbd148
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-07-09 17:05:06 +0300

    Implemented the logic but it doesn't work

--- CaesarCipher/CaesarCipher/VigenèreCipher.cs
+++ CaesarCipher/CaesarCipher/VigenèreCipher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,13 +16,55 @@ namespace CaesarCipher
 
         public override string Encryption()
         {
-            throw new NotImplementedException();
+            string encryptedMessage = string.Empty;
+
+            string modifiedKey = KeyManagement(_key, _text);
+
+            for (int i = 0; i < _text.Length; i++)
+            {
+                int x = (_text[i] + modifiedKey[i]) % Alphabet.Length;
+
+                x += 'A';
+                char ch = (char)(x);
+
+                encryptedMessage += ch;
+            }
+
+            return encryptedMessage;
         }
 
         public override string Decryption()
         {
-            throw new NotImplementedException();
+            string encryptedMessage = string.Empty;
+            string modifiedKey = KeyManagement(_key, _text);
+
+            for (int i = 0; i < _text.Length; i++)
+            {
+                int x = (_text[i] - modifiedKey[i]) % Alphabet.Length;
+
+                x += 'A';
+                char ch = (char)(x);
+
+                encryptedMessage += ch;
+            }
+
+            return encryptedMessage;
         }
 
+        //Repeating the key work until in match the secret text length
+        private string KeyManagement(string key, string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (key.Length == text.Length)
+                {
+                    break;
+                }
+
+                key += key[i];
+            }
+
+            return key;
+        }
     }
 }
