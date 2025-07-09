ed361a30ec1364c3b31dce3880b340c602e71e43
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-07-09 16:15:10 +0300

    Added new child class VigenèreCipher.cs

---
+++ CaesarCipher/CaesarCipher/VigenèreCipher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    public class VigenèreCipher : Cipher
+    {
+        public VigenèreCipher(string text, string key) : base(text, key)
+        {
+        }
+
+
+        public override string Encryption()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Decryption()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
