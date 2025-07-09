84fb82b20a0e83cd7e045de631311553831b9cd2
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-07-09 16:46:05 +0300

    Some modifications in the Program.cs to make the project work with the 2 ciphers
    After starting the Console App user can choose which cipher he wants

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -3,24 +3,6 @@ using TextCopy;
 
 Console.InputEncoding = System.Text.Encoding.Unicode;
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine(@"  ___ __ _  ___  ___  __ _ _ __        _       _               
- / __/ _` |/ _ \/ __|/ _` | '__|     (_)     | |              
-| (_| (_| |  __/\__ \ (_| | |     ___ _ _ __ | |__   ___ _ __ 
- \___\__,_|\___||___/\__,_|_|    / __| | '_ \| '_ \ / _ \ '__|
-                                 | (__| | |_) | | | |  __/ |   
-                                  \___|_| .__/|_| |_|\___|_|   
-                                        | |                    
-                                        |_|      ");
-
-Console.WriteLine(@".-------------------.---------------.----------.----------.---------------.
-|E N C R Y P T|===|M E S S A G E S|===|L I K E|===|R O M A N|===|E M P E R O R|™
-'-------------------'---------------'----------'----------'---------------'");
-
-Console.WriteLine();
-
-Console.ForegroundColor = ConsoleColor.White;
-
 Console.Write("English (0) or Bulgarian (1)?: ");
 int alphabetToggle = int.Parse(Console.ReadLine());
 
@@ -31,6 +13,18 @@ while (alphabetToggle < 0 || alphabetToggle > 1)
     Console.ForegroundColor = ConsoleColor.White;
     Console.Write("English (0) or Bulgarian (1)?: ");
     alphabetToggle = int.Parse(Console.ReadLine());
+}
+
+//Ask the user for which cipher he wants
+Console.Write("Choose cipher - Caesar Cipher (0) or Vigenère Cipher (1)?: ");
+int cipherToggle = int.Parse(Console.ReadLine());
+while (cipherToggle < 0 || cipherToggle > 1)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Your choose can be only 0 or 1 !");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("Choose cipher - Caesar Cipher(0) or Vigenère Cipher(1)?:");
+    cipherToggle = int.Parse(Console.ReadLine());
 }
 
 Console.Write("Enter your secret message: ");
@@ -45,16 +39,42 @@ while(string.IsNullOrEmpty(secret) || string.IsNullOrWhiteSpace(secret))
     secret = Console.ReadLine();
 }
 
-Console.Write("Enter your secret key (0-26): ");
-int key = int.Parse(Console.ReadLine());
+Console.Write("Enter your secret key: ");
+string key = Console.ReadLine();
 
-while (key < 0 || key > 26)
+switch (cipherToggle)
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Secret key number must be between 0 and 26 !");
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.Write("Enter your secret key (0-26): ");
-    key = int.Parse(Console.ReadLine());
+    //Caesar Cipher
+    case 0:
+        while (int.Parse(key) < 0 || int.Parse(key) > 26)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Secret key number must be between 0 and 26 !");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Enter your secret key: ");
+            key = Console.ReadLine();
+        }
+        break;
+    //Vigenère Cipher
+    case 1:
+        while (string.IsNullOrWhiteSpace(key.ToString()) || string.IsNullOrEmpty(key.ToString()))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Key can't be null or empty !");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Enter your secret key: ");
+            key = Console.ReadLine();
+        }
+
+        while (key.ToString() == secret)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Your key and secret message can't be the same!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Enter your secret key: ");
+            secret = Console.ReadLine();
+        }
+        break;
 }
 
 Console.Write("> Encryption (0) or Decryption (1)?: ");
@@ -72,32 +92,69 @@ while (toggle < 0 || toggle > 1)
 string englishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 string bulgarianAlphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЮЯ";
 
-CaesarCipher.CaesarCipher caesarCipher = new CaesarCipher.CaesarCipher(key.ToString(), secret);
-
-switch (alphabetToggle)
-{
-    //English
-    case 0:
-        caesarCipher.Alphabet = englishAlphabet;
-        break;
-    //Bulgarian
-    case 1:
-        caesarCipher.Alphabet = bulgarianAlphabet;
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
-        break;
-}
-
 string result = string.Empty;
-
-switch (toggle)
+switch (cipherToggle)
 {
-    //Encryption
+    //Caesar cipher
     case 0:
-        result = caesarCipher.Encryption();
+        CaesarCipher.CaesarCipher caesarCipher = new CaesarCipher.CaesarCipher(secret, key.ToString());
+
+        switch (alphabetToggle)
+        {
+            //English
+            case 0:
+                caesarCipher.Alphabet = englishAlphabet;
+                break;
+            //Bulgarian
+            case 1:
+                caesarCipher.Alphabet = bulgarianAlphabet;
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                break;
+        }
+
+        result = string.Empty;
+
+        switch (toggle)
+        {
+            //Encryption
+            case 0:
+                result = caesarCipher.Encryption();
+                break;
+            //Decryption
+            case 1:
+                result = caesarCipher.Decryption();
+                break;
+        }
         break;
-    //Decryption
     case 1:
-        result = caesarCipher.Decryption();
+        VigenèreCipher vigenèreCipher = new VigenèreCipher(secret, key.ToString());
+
+        switch (alphabetToggle)
+        {
+            //English
+            case 0:
+                vigenèreCipher.Alphabet = englishAlphabet;
+                break;
+            //Bulgarian
+            case 1:
+                vigenèreCipher.Alphabet = bulgarianAlphabet;
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                break;
+        }
+
+        result = string.Empty;
+
+        switch (toggle)
+        {
+            //Encryption
+            case 0:
+                result = vigenèreCipher.Encryption();
+                break;
+            //Decryption
+            case 1:
+                result = vigenèreCipher.Decryption();
+                break;
+        }
         break;
 }
 
